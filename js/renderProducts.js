1cc3e00c28f936adac2e681ba94605c8846796cc
Nicol Vanessa Corral <nicolvanessa2126@gmail.com>
2026-04-27 12:41:51 -0500

    fix: update URL path imports to work whit gh pages

--- js/renderProducts.js
+++ js/renderProducts.js
@@ -1,4 +1,4 @@
-import { getProducts } from "./products.js";
+import { getProducts } from "products.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const { products, error } = await getProducts();
